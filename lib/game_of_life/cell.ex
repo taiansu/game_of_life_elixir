48cccb70dffade3414e7bb57f9b713e0abfd156f
Tai An Su <i@taian.su>
2016-04-16 03:23:08 +0800

    Improve function args

--- lib/game_of_life/cell.ex
+++ lib/game_of_life/cell.ex
@@ -1,23 +1,23 @@
 defmodule Cell do
   @edge Application.get_env(:game_of_life, :edge)
 
-  def survive?({_, alive?} = cell, map) do
-    case n = neighbors_alive(cell, map) do
+  def survive?({coord, alive?}, map) do
+    case n = neighbors_alive(coord, map) do
       2 -> alive?
       3 -> true
       _ when n > -1 and n < @edge -> false
     end
   end
 
-  defp neighbors_alive(cell, map) do
-    cell
+  defp neighbors_alive(coord, map) do
+    coord
     |> neighbors
     |> Enum.reduce(0, fn neighbor, accu ->
         if map[neighbor], do: accu + 1, else: accu
        end)
   end
 
-  defp neighbors({{cell_x, cell_y}, _}) do
+  defp neighbors({cell_x, cell_y}) do
     for x <- -1..1, y <- -1..1, x != 0 or y != 0 do
       {cell_x + x, cell_y + y}
     end
